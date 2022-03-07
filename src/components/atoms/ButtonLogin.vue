d4f5ae4d6204dbeeb687f9e1bcd46bd51fb8d486
Grigory Morgachev <grinmorg@gmail.com>
2022-03-07 18:40:38 +0300

    update Sidebar

--- src/components/atoms/ButtonLogin.vue
+++ src/components/atoms/ButtonLogin.vue
@@ -1,8 +1,8 @@
 <template>
   <button
-    class="focus:outline-none flex items-center whitespace-nowrap px-2 py-1 text-sm text-blue-700 uppercase border border-blue-700 rounded-sm"
+    :class="classes"
   >
-    <BaseIcon name="user" class="mr-2 w-7 h-7"/>
+    <BaseIcon name="userCircle" class="mr-2 w-7 h-7"/>
     
     Sign In
   </button>
@@ -14,6 +14,11 @@ import BaseIcon from './base/BaseIcon.vue';
 export default {
   components: {
     BaseIcon
+  },
+  computed: {
+    classes() {
+      return this.$attrs.class || 'focus:outline-none flex items-center whitespace-nowrap px-2 py-1 text-sm text-blue-700 uppercase border border-blue-700 rounded-sm';
+    }
   }
 };
 </script>
